a7781a987ba4ec7028f751b8c1dc51a3e124b4a9
unknown <jchernandez827@gmail.com>
2021-07-13 16:28:39 -0500

    Create List Elements With JavaScript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,84 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
-const list = document.querySelector('.list-placeholder');
-// eslint-disable-next-line quotes
-list.innerHTML = `<li>sdfgsgfhhf</li>
-<li>sdfgsgfhhf</li>
-<li>sdfgsgfhhf</li>`;
+const myArr = [
+  {
+    description: 'Walk the dog 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Walk the dog 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Walk the dog 3',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Walk the dog 4',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Walk the dog 5',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Walk the dog 6',
+    completed: false,
+    index: 5,
+  },
+];
+
+const list = document.querySelector('.placeholder');
+const unList = document.createElement('div');
+unList.classList.add('container');
+list.appendChild(unList);
+const title = document.createElement('h1');
+title.textContent = "Today's To Do";
+title.classList.add('title');
+unList.appendChild(title);
+const addList = document.createElement('input');
+addList.setAttribute('type', 'text');
+addList.placeholder = 'Add to your list...';
+addList.classList.add('add-list');
+unList.appendChild(addList);
+
+function display() {
+  for (let i = 0; i < myArr.length; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+    row.appendChild(textContainer);
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    textContainer.appendChild(checkBox);
+    const text = document.createElement('span');
+    text.textContent = myArr[i].description;
+    text.classList.add('text');
+    textContainer.appendChild(text);
+    const icon = document.createElement('i');
+    icon.textContent = '::';
+    icon.classList.add('row-icon');
+    row.appendChild(icon);
+    unList.appendChild(row);
+  }
+  const footer = document.createElement('div');
+  footer.classList.add('footer');
+  const footerBtn = document.createElement('button');
+  footerBtn.setAttribute('type', 'button');
+  footerBtn.classList.add('footer-btn');
+  footerBtn.textContent = 'Clear All Completed';
+  footer.appendChild(footerBtn);
+  unList.appendChild(footer);
+}
+
+display();
